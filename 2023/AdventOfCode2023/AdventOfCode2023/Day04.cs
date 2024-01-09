5cccae8536aad51c2c244525539f450c692dc7e1
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-01-09 16:24:22 +0100

    Separating winning and card numbers

--- 2023/AdventOfCode2023/AdventOfCode2023/Day04.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Day04.cs
@@ -5,8 +5,8 @@ namespace AdventOfCode2023_1;
 public static class Day04
 {
     private static readonly string FilePath = Path.Combine(Constants.RootInputPath, "/Day04/Day04.in");
-    private static readonly string MockFilePath = Path.Combine(Constants.RootInputPath, "/Day04/MockDay04.in");
-    private static readonly string FullPath = Path.Combine(Directory.GetCurrentDirectory(), MockFilePath);
+    private static readonly string MockFilePath = Constants.RootInputPath + "/Day04/MockDay04.in";
+    private static readonly string FullPath = Directory.GetCurrentDirectory() + MockFilePath;
     private static readonly string InputFile = File.ReadAllText(FullPath);
     private static readonly List<string> Input = SharedMethods.GetInput(InputFile);
 
@@ -20,6 +20,49 @@ public static class Day04
 
     private static void PartOne()
     {
-        throw new NotImplementedException();
+        var result = GetSumScratchCardPoints();
+        SharedMethods.AnswerPart(1, result);
+    }
+
+    #region Part 1
+    private static int GetSumScratchCardPoints()
+    {
+        return GetScratchCardPoints().Sum();
+    }
+
+    private static List<int> GetScratchCardPoints()
+    {
+        var scratchCards = GetScratchCards();
+
+        return scratchCards.Select(card => card.Points).ToList();
+    }
+
+    private static List<ScratchCard> GetScratchCards()
+    {
+        var scratchCards = new List<ScratchCard>();
+        foreach (var line in Input)
+        {
+            var gameNumbers = line.Split(Constants.Colon).ToList().Last();
+            var winningNumbers = gameNumbers.Split(Constants.Pipe).ToList().First().Trim();
+            var cardNumbers = gameNumbers.Split(Constants.Pipe).ToList().Last().Trim();
+            var scratchCard = new ScratchCard(winningNumbers, cardNumbers);
+            scratchCards.Add(scratchCard);
+        }
+
+        return scratchCards;
+    }
+    #endregion
+    
+    private class ScratchCard(string winningNumbers, string cardNumbers)
+    {
+        public List<int> WinningNumbers { get; set; } = ConvertToList(winningNumbers);
+        public List<int> CardNumbers { get; set; } = ConvertToList(cardNumbers);
+        public int Points { get; set; }
+
+        private static List<int> ConvertToList(string inputString)
+        {
+            var separatedString = inputString.Split(Constants.Space).ToList();
+            return separatedString.Where(number => int.TryParse(number, out _)).Select(int.Parse).ToList();
+        }
     }
 }
