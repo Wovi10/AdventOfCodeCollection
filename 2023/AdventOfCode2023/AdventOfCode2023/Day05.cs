4af84c02074886a7a771f28906092b6fbcc96fe8
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-01-10 17:42:20 +0100

    Converting to soils

--- 2023/AdventOfCode2023/AdventOfCode2023/Day05.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Day05.cs
@@ -6,6 +6,54 @@ public class Day05 : DayBase
 {
     private static readonly List<string> Input = SharedMethods.GetInput("05", true);
 
+    private static List<List<List<int>>> InputParts = GenerateInputParts();
+
+    private static List<List<List<int>>> GenerateInputParts()
+    {
+        var inputParts = new List<List<string>>();
+        var inputPart = new List<string>();
+        foreach (var line in Input)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+                inputPart.Add(line.Trim());
+            else
+            {
+                inputParts.Add(inputPart);
+                inputPart = [];
+            }
+        }
+
+        for (var i = 1; i < inputParts.Count; i++)
+        {
+            inputParts[i].RemoveAt(0);
+        }
+
+        var result = GenerateMappings(inputParts); 
+        return result;
+    }
+
+    private static List<List<List<int>>> GenerateMappings(List<List<string>> inputPartsAsStrings)
+    {
+        var result = new List<List<List<int>>>();
+        for (var i = 1; i < inputPartsAsStrings.Count; i++)
+        {
+            var inputPart = inputPartsAsStrings[i];
+            var convertedInputPart = inputPart
+                    .Select(line =>
+                    {
+                        return line
+                            .Split(Constants.Space)
+                            .Where(location => int.TryParse(location, out _))
+                            .Select(int.Parse)
+                            .ToList();
+                    })
+                    .ToList();
+            result.Add(convertedInputPart);
+        }
+
+        return result;
+    }
+    
     public override void PartOne()
     {
         var result = GetLowestLocationNumber();
@@ -15,10 +63,16 @@ public class Day05 : DayBase
     private static int GetLowestLocationNumber()
     {
         var seeds = GetSeeds();
-
+        var soilsOptions = ConvertToSoilOptions(seeds);
         return seeds.Min();
     }
 
+    public override void PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    #region Part1
     private static List<int> GetSeeds()
     {
         var seeds = Input
@@ -32,8 +86,53 @@ public class Day05 : DayBase
         return seeds.Where(seed => int.TryParse(seed, out _)).Select(int.Parse).ToList();
     }
 
-    public override void PartTwo()
+    private static List<int> ConvertToSoilOptions(List<int> seeds)
     {
-        throw new NotImplementedException();
+        var soils = new List<int>();
+        foreach (var seed in seeds)
+        {
+            soils.AddRange(GetSoils(seed));
+        }
+
+        return soils;
     }
+
+    private static List<int> GetSoils(int initialSeed)
+    {
+        var soils = new List<int>();
+        for (var i = 0; i < InputParts[0].Count; i++)
+        {
+            var seed = initialSeed;
+            var source = GetSource(InputParts[0][i]);
+            var destination = GetDestination(InputParts[0][i]);
+            var range = GetRange(InputParts[0][i]);
+            if (seed >= int.Min(int.Min(source, source + range), int.Min(destination, destination + range)) 
+                && 
+                seed <= int.Max(int.Max(source, source + range), int.Max(destination, destination + range)))
+            {
+                seed += range;
+            }
+
+            soils.Add(seed);
+        }
+
+        return soils;
+    }
+
+    private static int GetDestination(List<int> inputPart)
+    {
+        return inputPart[0];
+    }
+
+    private static int GetRange(List<int> inputPart)
+    {
+        return inputPart[2];
+    }
+
+    private static int GetSource(List<int> inputPart)
+    {
+        return inputPart[1];
+    }
+
+    #endregion
 }
