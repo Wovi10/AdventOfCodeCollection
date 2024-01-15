9820ac2500c0733360266de0b715c84aa9dee4ba
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-01-15 17:07:27 +0100

    Done with part 1

--- 2023/AdventOfCode2023/AdventOfCode2023/Day05.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Day05.cs
@@ -1,114 +1,27 @@
-﻿using AdventOfCode2023_1.Shared;
+﻿using AdventOfCode2023_1.Models;
+using AdventOfCode2023_1.Shared;
 
 namespace AdventOfCode2023_1;
 
 public class Day05 : DayBase
 {
     private static readonly List<string> Input = SharedMethods.GetInput("05");
-
-    private static readonly List<List<List<long>>> InputParts = GenerateInputParts();
-
-    private static List<List<List<long>>> GenerateInputParts()
-    {
-        var inputParts = new List<List<string>>();
-        var inputPart = new List<string>();
-        foreach (var line in Input)
-        {
-            if (!string.IsNullOrWhiteSpace(line))
-                inputPart.Add(line.Trim());
-            else
-            {
-                inputParts.Add(inputPart);
-                inputPart = new List<string>();
-            }
-        }
-        inputParts.Add(inputPart);
-
-        for (var i = 1; i < inputParts.Count; i++) 
-            inputParts[i].RemoveAt(0);
-
-        var result = GenerateMappings(inputParts); 
-        return result;
-    }
-
-    private static List<List<List<long>>> GenerateMappings(List<List<string>> inputPartsAsStrings)
-    {
-        var result = new List<List<List<long>>>();
-        for (var i = 1; i < inputPartsAsStrings.Count; i++)
-        {
-            var inputPart = inputPartsAsStrings[i];
-            var convertedInputPart = inputPart
-                    .Select(line =>
-                    {
-                        return line
-                            .Split(Constants.Space)
-                            .Where(location => long.TryParse(location, out _))
-                            .Select(long.Parse)
-                            .ToList();
-                    })
-                    .ToList();
-            result.Add(convertedInputPart);
-        }
-
-        return result;
-    }
     
+    private readonly List<long> _seedsToTest = new();
+
+    private readonly List<SeedMapping> _seedToSoil = new();
+    private readonly List<SeedMapping> _soilToFert = new();
+    private readonly List<SeedMapping> _fertToWater = new();
+    private readonly List<SeedMapping> _waterToLight = new();
+    private readonly List<SeedMapping> _lightToTemp = new();
+    private readonly List<SeedMapping> _tempToHumid = new();
+    private readonly List<SeedMapping> _humidToLoc = new();
+
     public override void PartOne()
     {
+        
         var result = GetLowestLocationNumber();
         SharedMethods.AnswerPart(1, result);
-    }
-
-    private static long GetLowestLocationNumber()
-    {
-        long? possibility = null;
-        var seeds = GetSeeds();
-        var counter = 0;
-        foreach (var seed in seeds.Distinct())
-        {
-            counter++;
-            var soils = GetConvertedValue(seed, 0);
-            foreach (var soil in soils)
-            {
-                var fertilizers = GetConvertedValue(soil, 1);
-                foreach (var fertilizer in fertilizers)
-                {
-                    var waters = GetConvertedValue(fertilizer, 2);
-                    foreach (var water in waters)
-                    {
-                        var lights = GetConvertedValue(water, 3);
-                        foreach (var light in lights)
-                        {
-                            var temperatures = GetConvertedValue(light, 4);
-                            foreach (var temperature in temperatures)
-                            {
-                                var humidities = GetConvertedValue(temperature, 5);
-                                foreach (var humidity in humidities)
-                                {
-                                    var locations = GetConvertedValue(humidity, 6);
-                                    var newPossibility = locations.Min();
-                                    possibility = possibility == null 
-                                        ? newPossibility 
-                                        : Math.Min(possibility.Value, newPossibility);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine($"Seed {counter} of {seeds.Count} done");
-        }
-
-        return possibility ?? 0;
-        // var soilsOptions = ConvertSourceToDestination(seeds, 0);
-        // var fertilizerOptions = ConvertSourceToDestination(soilsOptions, 1);
-        // var waterOptions = ConvertSourceToDestination(fertilizerOptions, 2);
-        // var lightOptions = ConvertSourceToDestination(waterOptions, 3);
-        // var temperatureOptions = ConvertSourceToDestination(lightOptions, 4);
-        // var humidityOptions = ConvertSourceToDestination(temperatureOptions, 5);
-        // var locationOptions = ConvertSourceToDestination(humidityOptions, 6);
-        // return locationOptions.Min();
     }
 
     public override void PartTwo()
@@ -117,73 +30,81 @@ public class Day05 : DayBase
     }
 
     #region Part1
-    private static List<long> GetSeeds()
-    {
-        var seeds = Input
-            .First() // First line
-            .Split(Constants.Colon)
-            .Last() // Everything after colon
-            .Trim()
-            .Split(Constants.Space)
-            .ToList();
 
-        return seeds.Where(seed => long.TryParse(seed, out _)).Select(long.Parse).ToList();
+    private long GetLowestLocationNumber()
+    {
+        ProcessFile();
+        return SearchLowestLocation() ?? 0;
     }
 
-    private static List<long> ConvertSourceToDestination(List<long> sourceList, int inputPartsIndex)
+    private void ProcessFile()
     {
-        var destinationList = new List<long>();
-        foreach (var source in sourceList)
+        List<SeedMapping>? currentList = null;
+
+        foreach (var line in Input.Where(line => !TryAddSeed(line)))
         {
-            destinationList.AddRange(GetConvertedValue(source, inputPartsIndex));
+            currentList = GetMappingList(line) ?? currentList;
+
+            if (currentList != null) 
+                AddSeedMapping(currentList, line);
+        }
+    }
+
+    private bool TryAddSeed(string line)
+    {
+        if (!line.StartsWith("seeds:")) return false;
+
+        _seedsToTest.AddRange(line[7..].Split(Constants.Space).Select(long.Parse));
+        return true;
+
+    }
+
+    private List<SeedMapping>? GetMappingList(string line)
+    {
+        return line switch
+        {
+            "seed-to-soil map:" => _seedToSoil,
+            "soil-to-fertilizer map:" => _soilToFert,
+            "fertilizer-to-water map:" => _fertToWater,
+            "water-to-light map:" => _waterToLight,
+            "light-to-temperature map:" => _lightToTemp,
+            "temperature-to-humidity map:" => _tempToHumid,
+            "humidity-to-location map:" => _humidToLoc,
+            _ => null
+        };
+    }
+
+    private static void AddSeedMapping(List<SeedMapping> currentList, string line)
+    {
+        var parts = line.Split(Constants.Space).Select(long.Parse).ToArray();
+        currentList.Add(new SeedMapping(parts[1], parts[0], parts[2]));
+    }
+
+    private long? SearchLowestLocation()
+    {
+        long? lowest = null;
+
+        foreach (var seed in _seedsToTest)
+        {
+            //AnsiConsole.WriteLine("Testing seed " + seed + " for lowest location");
+            var result = TestLocation(seed, _seedToSoil);
+            result = TestLocation(result, _soilToFert);
+            result = TestLocation(result, _fertToWater);
+            result = TestLocation(result, _waterToLight);
+            result = TestLocation(result, _lightToTemp);
+            result = TestLocation(result, _tempToHumid);
+            result = TestLocation(result, _humidToLoc);
+            if (lowest == null || result < lowest)
+                lowest = result;
         }
 
-        return destinationList;
+        return lowest;
     }
 
-    private static IEnumerable<long> GetConvertedValue(long initialLocation, int inputPartsIndex)
+    private static long TestLocation(long seed, List<SeedMapping> mappings)
     {
-        var destinationList = new List<long>();
-        var inputPart = InputParts[inputPartsIndex];
-        foreach (var inputPartLine in inputPart)
-        {
-            var newLocation = initialLocation;
-
-            var source = GetSource(inputPartLine);
-            var destination = GetDestination(inputPartLine);
-            var range = GetRange(inputPartLine);
-
-            var biggest = long.Max(source, destination);
-            var smallest = long.Min(source, destination);
-
-            if (newLocation >= smallest && newLocation <= biggest + range)
-            {
-                if (biggest == source)
-                    newLocation += range;
-                else
-                    newLocation -= range;
-            }
-
-            if (newLocation > 0) 
-                destinationList.Add(newLocation);
-        }
-
-        return destinationList;
-    }
-
-    private static long GetDestination(List<long> inputPart)
-    {
-        return inputPart[0];
-    }
-
-    private static long GetRange(List<long> inputPart)
-    {
-        return inputPart[2];
-    }
-
-    private static long GetSource(List<long> inputPart)
-    {
-        return inputPart[1];
+        var mapping = mappings.FirstOrDefault(x => x.IsInRange(seed));
+        return mapping?.MapValue(seed) ?? seed;
     }
 
     #endregion
