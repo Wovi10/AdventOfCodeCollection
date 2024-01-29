677463d27e4d5ebec8e99a2b6f1a26a3af1db226
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-01-29 16:23:53 +0100

    Resolve conflicts

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Day06.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2023_1;
+
+public class Day06:DayBase
+{
+    protected override void PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Run(int i, string waitForIt)
+    {
+        throw new NotImplementedException();
+    }
+}
--- 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -2,6 +2,9 @@
 
 using AdventOfCode2023_1;
 
-// Day01.Run();
-// Day02.Run();
-Day03.Run();
+// new Day01().Run(1,"Trebuchet?!");
+// new Day02().Run(2,"Cube Conundrum");
+// new Day03().Run(3, "Gear Ratios");
+// new Day04().Run(4, "Scratchcards");
+// new Day05().Run(5, "If You Give A Seed A Fertilizer");
+new Day06().Run(6, "Wait For It");
