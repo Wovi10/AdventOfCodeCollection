cad1fed94f4f41701ecbdca44ba98a931c8fc4b4
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-01-31 17:29:38 +0100

    Almost got part 2 working

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Models/Day08/MathUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2023_1.Models.Day08;
+
+public class MathUtil
+{
+    public static int Gcd(int a, int b)
+    {
+        if (a == 0 && b == 0)
+            return 1;
+        return b == 0 
+            ? a 
+            : Gcd(b, a % b);
+    }
+
+    public static int Lcm(int a, int b) {
+        return Math.Abs(a * b) / Gcd(a, b);
+    }
+}
