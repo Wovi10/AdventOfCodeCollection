d14d08bd1e9e9ef0bcf3bbc69c5c903632e21391
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-03-26 15:18:43 +0100

    Finished part 1

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Models/Day14/Day14Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2023_1.Models.Day14;
+
+public static class Day14Extensions
+{
+    public static RockType ToRockType(this char rockChar)
+    {
+        return rockChar switch
+        {
+            '.' => RockType.None,
+            'O' => RockType.Round,
+            '#' => RockType.Square,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Models/Day14/DishRow.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2023_1.Models.Day14;
+
+public class DishRow
+{
+    public List<RockType> Rocks { get; set; } = new();
+
+    public DishRow(string line)
+    {
+        foreach (var rockChar in line)
+        {
+            Rocks.Add(rockChar.ToRockType());
+        }
+    }
+
+    public List<int> GetRoundRocksIndices()
+        => Rocks.Select((r, i) => r == RockType.Round ? i : -1).Where(i => i != -1).ToList();
+
+    public List<int> GetSquareRocksIndices()
+        => Rocks.Select((r, i) => r == RockType.Square ? i : -1).Where(i => i != -1).ToList();
+
+    public List<int> GetNoneRocksIndices()
+        => Rocks.Select((r, i) => r == RockType.None ? i : -1).Where(i => i != -1).ToList();
+    
+    public List<int> GetBlockedIndices() 
+        => Rocks.Select((r, i) => r != RockType.None ? i : -1).Where(i => i != -1).ToList();
+}
