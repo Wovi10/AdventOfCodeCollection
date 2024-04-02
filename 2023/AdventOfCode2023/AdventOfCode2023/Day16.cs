38b313a9f3b947ed946d2c982de456a06632b9ca
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-04-02 11:05:45 +0200

    Preparing day 16

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Day16.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2023_1;
+
+public class Day16 : DayBase
+{
+    protected override Task PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
