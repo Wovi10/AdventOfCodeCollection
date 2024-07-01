3825d8c83aa0bff0ae2d8b90cda56382f7128d2c
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-07-01 15:33:46 +0200

    Started day 21

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Models/Day21/Enums/TileType.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2023_1.Models.Day21.Enums;
+
+public enum TileType
+{
+    StartingPosition,
+    GardenPlot,
+    Rock
+}
+
+public static class TileTypeExtensions
+{
+    public static TileType ToTileType(this char tileTypeInput)
+    {
+        return tileTypeInput switch
+        {
+            'S' => TileType.StartingPosition,
+            '.' => TileType.GardenPlot,
+            '#' => TileType.Rock,
+            _ => throw new NotImplementedException("UnexpectedInput")
+        };
+    }
+}
---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Models/Day21/Tile.cs
@@ -0,0 +1,22 @@
+﻿using UtilsCSharp.Enums;
+using UtilsCSharp.Objects;
+
+namespace AdventOfCode2023_1.Models.Day21;
+
+public class Tile(int internalX, int internalY) : NodeBase<int>(internalX, internalY)
+{
+    private readonly int _internalX = internalX;
+    private readonly int _internalY = internalY;
+
+    public override (int, int) Move(Direction direction, int distance = 1)
+    {
+        return direction switch
+        {
+            Direction.Up => (_internalX, _internalY - distance),
+            Direction.Right => (_internalX + distance, _internalY),
+            Direction.Down => (_internalX, _internalY + distance),
+            Direction.Left => (_internalX - distance, _internalY),
+            _ => (_internalX, _internalY)
+        };
+    }
+}
