9e4c1abf10b802b45ae2aed1ee2fccecc9cd6a8a
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-12-16 09:37:56 +0100

    Quick save

--- AdventOfCode/2024/Models/Day08/Coordinate.cs
+++ AdventOfCode/2024/Models/Day08/Coordinate.cs
@@ -10,6 +10,9 @@ public class Coordinate(int x, int y): NodeBase<int>(x, y)
         throw new NotImplementedException();
     }
 
-    public int DistanceTo(Coordinate other)
-        => (X - other.X) + (Y - other.Y);
+    public (int, int) DistanceTo(Coordinate other)
+        => (X - other.X, Y - other.Y);
+
+    public (int, int) Move(Coordinate otherCoordinate)
+        => (X + otherCoordinate.X, Y + otherCoordinate.Y);
 }
--- AdventOfCode/2024/Models/Day08/Location.cs
+++ AdventOfCode/2024/Models/Day08/Location.cs
@@ -6,4 +6,10 @@ public class Location
     public Coordinate Coordinate { get; set; }
     public bool IsAntenna { get; set; }
     public char? Frequency { get; set; }
+
+    public (int, int) DistanceTo(Location other)
+        => Coordinate.DistanceTo(other.Coordinate);
+
+    public (int, int) Move(Location other)
+        => Coordinate.Move(other.Coordinate);
 }
