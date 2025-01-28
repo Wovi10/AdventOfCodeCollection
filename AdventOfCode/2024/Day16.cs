58f89549f99d996aabd356d032827029e3d8eff1
Wout Vinckevleugel <wout.vinck@gmail.com>
2025-01-28 10:21:56 +0100

    Setting up day 16

---
+++ AdventOfCode/2024/Day16.cs
@@ -0,0 +1,16 @@
+﻿using AOC.Utils;
+
+namespace _2024;
+
+public class Day16(): DayBase("16", "Reindeer Maze")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventOfCode/2024/Models/Day16/Day16Extensions.cs
@@ -0,0 +1,6 @@
+﻿namespace _2024.Models.Day16;
+
+public static class Day16Extensions
+{
+    
+}
