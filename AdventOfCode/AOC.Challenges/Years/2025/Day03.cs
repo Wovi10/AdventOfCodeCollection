1771f86f109801665beb1c376aaa5a46aca66ebe
Wovi10 <wout.vinckevleugel@gmail.com>
2026-07-29 21:54:26 +0200

    03/2025 Stuck, end of day

--- AdventOfCode/AOC.Challenges/Years/2025/Day03.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day03.cs
@@ -15,7 +15,7 @@ public class Day03() : DayBase("03", "Lobby")
     {
         var result = GetTotalJoltage();
 
-        return Task.FromResult<object>(result);
+        return Task.FromResult<object>(result); // > 170775065764953
     }
 
     private long GetTotalJoltage()
@@ -25,18 +25,33 @@ public class Day03() : DayBase("03", "Lobby")
         return input.Sum(GetJoltage);
     }
 
-    private int GetJoltage(string line)
+    private const int JoltageLength = 12;
+    private long GetJoltage(string line)
     {
         if (Variables.RunningPartOne)
         {
-            var usableNumbers = line[..^1].ToCharArray();
+            var usableNumbers = line[..^1];
             var highestNumber = usableNumbers.Max();
             var indexHighest = line.IndexOf(highestNumber) + 1;
-            var secondHighest = line[indexHighest..].ToCharArray().Max();
+            var secondHighest = line[indexHighest..].Max();
 
-            return int.Parse($"{highestNumber}{secondHighest}");
+            return long.Parse($"{highestNumber}{secondHighest}");
         }
 
-        return 0;
+        var maxIndexNumber = line.Length - JoltageLength;
+        var highest = line[..maxIndexNumber].Max();
+        var indexHigh = line.IndexOf(highest);
+        var leftOver = line[indexHigh..];
+
+        var length = leftOver.Length;
+        while (length > JoltageLength)
+        {
+            var lowest = leftOver.Min();
+            leftOver = leftOver.Remove(leftOver.IndexOf(lowest), 1);
+
+            length = leftOver.Length;
+        }
+
+        return long.Parse(leftOver);
     }
 }
