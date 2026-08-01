594b28948b69ad9686d88bd5c2fb79f0e4103c9f
Wovi10 <wout.vinckevleugel@gmail.com>
2026-08-01 22:11:21 +0200

    04/2025 Prepared files

---
+++ AdventOfCode/AOC.Challenges/Years/2025/Day04.cs
@@ -0,0 +1,16 @@
+using AOC.Utils;
+
+namespace AOC.Challenges.Years._2025;
+
+public class Day04() : DayBase("04", "Printing Department")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
