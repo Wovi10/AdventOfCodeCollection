f8ba1cc5767340577047054e5793584740b56797
Wovi10 <wout.vinckevleugel@gmail.com>
2026-08-02 10:12:47 +0200

    05/2025 Found part 1

--- AdventOfCode/AOC.Challenges/Years/2025/Day05.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day05.cs
@@ -1,4 +1,5 @@
 using AOC.Utils;
+using UtilsCSharp;
 
 namespace AOC.Challenges.Years._2025;
 
@@ -18,6 +19,32 @@ public class Day05() : DayBase("05", "Cafeteria")
 
     private long CountFreshIngredients()
     {
-        throw new NotImplementedException();
+        var input = GetInput();
+
+        var doingRanges = true;
+        var ranges = new List<(long low, long high)>();
+        var result = 0L;
+        foreach (var line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                // All ranges done, start finding fresh ids
+                doingRanges = false;
+                continue;
+            }
+
+            if (doingRanges)
+            {
+                var bounds = line.Split("-");
+                ranges.Add(new (long.Parse(bounds[0]), long.Parse(bounds[1])));
+
+                continue;
+            }
+
+            var number = long.Parse(line);
+            result += ranges.Any(r => r.low <= number && r.high >= number) ? 1 : 0;
+        }
+
+        return result;
     }
 }
