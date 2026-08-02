31bfe3106a2e5d8b77be6b9d7293b57a247d17f3
Wovi10 <wout.vinckevleugel@gmail.com>
2026-08-02 10:32:02 +0200

    05/2025 Found a solution for part 2, doesn't work on real input because of data size

--- AdventOfCode/AOC.Challenges/Years/2025/Day05.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day05.cs
@@ -14,7 +14,9 @@ public class Day05() : DayBase("05", "Cafeteria")
 
     protected override Task<object> PartTwo()
     {
-        throw new NotImplementedException();
+        long result = TotalCountFreshIngredients();
+
+        return Task.FromResult<object>(result);
     }
 
     private long CountFreshIngredients()
@@ -46,5 +48,37 @@ public class Day05() : DayBase("05", "Cafeteria")
         }
 
         return result;
+    }
+
+    private long TotalCountFreshIngredients()
+    {
+        var input = GetInput();
+
+        var allBounds = new List<(long low, long high)>();
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                return CountTotal(allBounds);
+
+            var bounds = line.Split("-");
+
+            var lowerBound = long.Parse(bounds[0]);
+            var upperBound = long.Parse(bounds[1]);
+
+            if (allBounds.Any(b => b.low <= lowerBound && b.high >= upperBound))
+                continue;
+
+            allBounds = allBounds.Except(allBounds.Where(b => b.low > lowerBound && b.high < upperBound)).ToList();
+            allBounds.Add((lowerBound, upperBound));
+
+            // Only leaves bounds that are totally separate or overlap on 1 side
+        }
+
+        return 0;
+    }
+
+    private long CountTotal(List<(long low, long high)> bounds)
+    {
+        throw new NotImplementedException();
     }
 }
