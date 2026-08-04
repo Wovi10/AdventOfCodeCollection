5863e20b883f91aa67bbfb95a77e5fff1ba1d895
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-04 14:51:23 +0200

    06/2025 Prepared day 6

---
+++ AdventOfCode/AOC.Challenges/Years/2025/Day06.cs
@@ -0,0 +1,16 @@
+using AOC.Utils;
+
+namespace AOC.Challenges.Years._2025;
+
+public class Day06() : DayBase("06", "Trash Compactor")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
