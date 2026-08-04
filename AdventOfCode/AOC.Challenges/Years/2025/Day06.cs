ba19bda3e83695bd62060a0ff21843dbe5f50e53
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-04 15:14:22 +0200

    06/2025 Solved part 1

--- AdventOfCode/AOC.Challenges/Years/2025/Day06.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day06.cs
@@ -6,11 +6,39 @@ public class Day06() : DayBase("06", "Trash Compactor")
 {
     protected override Task<object> PartOne()
     {
-        throw new NotImplementedException();
+        var result = GetGrandTotalCephalopodMath();
+
+        return Task.FromResult<object>(result);
     }
 
     protected override Task<object> PartTwo()
     {
         throw new NotImplementedException();
+    }
+
+    private long GetGrandTotalCephalopodMath()
+    {
+        var input =
+            GetInput()
+                .Select(l => l.Split(" ").Where(item => !string.IsNullOrWhiteSpace(item) && item != " ").ToArray())
+            .ToArray();
+        var length = input.MaxBy(s => s.Length)?.Length ?? 0;
+
+        var result = 0L;
+        for (var i = 0; i < length; i++)
+        {
+            var allToUse = input.Select(l => l.GetValue(i)?.ToString() ?? "")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .ToArray();
+
+            var symbol = allToUse.Last();
+            var allNumbers = allToUse[..^1].Select(long.Parse).ToArray();
+
+            result += symbol == "+"
+                ? allNumbers.Sum()
+                : allNumbers.Aggregate(1L, (a, b) => a * b);
+        }
+
+        return result;
     }
 }
