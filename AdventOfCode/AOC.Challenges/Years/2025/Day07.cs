bdc960051174b866c4234f5a9db294c5f446bd7f
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-04 17:00:06 +0200

    07/2025 Finished part 1

--- AdventOfCode/AOC.Challenges/Years/2025/Day07.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day07.cs
@@ -6,11 +6,43 @@ public class Day07() : DayBase("07", "Laboratories")
 {
     protected override Task<object> PartOne()
     {
-        throw new NotImplementedException();
+        var result = CountOfLaserSplits();
+
+        return Task.FromResult<object>(result);
     }
 
     protected override Task<object> PartTwo()
     {
         throw new NotImplementedException();
+    }
+
+    private long CountOfLaserSplits()
+    {
+        var input = GetInput().ToArray();
+        var numberOfSplits = 0L;
+
+        var laserIndeces = new HashSet<int> { input.First().IndexOf('S') };
+        for (var i = 1; i < input.Length; i++)
+        {
+            var line = input[i];
+            if (!line.Contains('^'))
+                continue;
+
+            var nextIndeces = new  HashSet<int>();
+            foreach (var laserIndex in laserIndeces)
+            {
+                if (line[laserIndex] != '^')
+                {
+                    nextIndeces.Add(laserIndex);
+                    continue;
+                }
+
+                numberOfSplits++;
+                nextIndeces.Add(laserIndex - 1);
+                nextIndeces.Add(laserIndex + 1);
+            }
+            laserIndeces = nextIndeces;
+        }
+        return numberOfSplits;
     }
 }
