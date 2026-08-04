ed25dbb0bcdac91048ca0ed3ce57d4c88aa30e97
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-04 16:43:02 +0200

    07/2025 Prepared day 7

---
+++ AdventOfCode/AOC.Challenges/Years/2025/Day07.cs
@@ -0,0 +1,16 @@
+using AOC.Utils;
+
+namespace AOC.Challenges.Years._2025;
+
+public class Day07() : DayBase("07", "Laboratories")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
