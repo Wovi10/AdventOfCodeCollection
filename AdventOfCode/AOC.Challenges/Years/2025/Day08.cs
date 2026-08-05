821f661819e8fa85ccc1aa90dab93790adb2964e
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-05 08:32:36 +0200

    08/2025 Prepared day 8

---
+++ AdventOfCode/AOC.Challenges/Years/2025/Day08.cs
@@ -0,0 +1,16 @@
+using AOC.Utils;
+
+namespace AOC.Challenges.Years._2025;
+
+public class Day08() : DayBase("08", "Playground")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
