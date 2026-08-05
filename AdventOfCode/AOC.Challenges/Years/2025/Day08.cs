f114d3acae020a105d1896b237e146d3b8831d58
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-05 10:49:43 +0200

    08/2025 SOlved part 2

--- AdventOfCode/AOC.Challenges/Years/2025/Day08.cs
+++ AdventOfCode/AOC.Challenges/Years/2025/Day08.cs
@@ -1,8 +1,9 @@
 using AOC.Utils;
+using NLog;
 
 namespace AOC.Challenges.Years._2025;
 
-public class Day08() : DayBase("08", "Playground")
+public class Day08() : DayBase("08", "Playground", LogManager.GetCurrentClassLogger())
 {
     protected override Task<object> PartOne()
     {
@@ -13,13 +14,17 @@ public class Day08() : DayBase("08", "Playground")
 
     protected override Task<object> PartTwo()
     {
-        throw new NotImplementedException();
+        var result = DistanceToWallForLastTwo();
+
+        return Task.FromResult<object>(result);
     }
 
     private double MulLargestCircuits()
     {
+        var input = GetInput().Select(x => x.Split(',').Select(long.Parse).ToArray()).ToArray();
         var numOfConnectionsToMake = Variables.UseMockInput == true ? 10 : 1000;
-        var distancesBetweenBoxes = GetDistanceBetweenBoxes(numOfConnectionsToMake).OrderBy(x => x.dist).Take(numOfConnectionsToMake).ToArray();
+        var distancesBetweenBoxes = GetDistanceBetweenBoxes(input, numOfConnectionsToMake).OrderBy(x => x.dist).Take
+            (numOfConnectionsToMake).ToArray();
 
         var circuits = new List<HashSet<int>>();
         foreach (var (_, index1, index2) in distancesBetweenBoxes)
@@ -57,11 +62,12 @@ public class Day08() : DayBase("08", "Playground")
         return circuits.Take(3).Aggregate(1L, (current, next) => current * next.Count);
     }
 
-    private List<(double dist, int index1, int index2)> GetDistanceBetweenBoxes(int numOfConnectionsToMake)
+    private List<(double dist, int index1, int index2)> GetDistanceBetweenBoxes(long[][] input,
+        long numOfConnectionsToMake)
     {
         var result = new List<(double dist, int index1, int index2)>();
 
-        var input = GetInput().Select(x => x.Split(',').Select(int.Parse).ToArray()).ToArray();
+
         for (var i = 0; i < input.Length; i++)
         {
             var (x1, y1, z1) = (input[i][0], input[i][1], input[i][2]);
@@ -78,7 +84,53 @@ public class Day08() : DayBase("08", "Playground")
         }
         return result;
 
-        static double CalcDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+        static double CalcDistance(long x1, long y1, long z1, long x2, long y2, long z2)
             => Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+    }
+
+    private long DistanceToWallForLastTwo()
+    {
+        var input = GetInput().Select(x => x.Split(',').Select(long.Parse).ToArray()).ToArray();
+        var distancesBetweenBoxes = GetDistanceBetweenBoxes(input, long.MaxValue).OrderBy(x => x.dist).ToArray();
+
+        var circuits = new List<HashSet<int>>();
+        foreach (var (_, index1, index2) in distancesBetweenBoxes)
+        {
+            var circuitWithIndex1 = circuits.FirstOrDefault(c => c.Contains(index1));
+            var circuitWithIndex2 = circuits.FirstOrDefault(c => c.Contains(index2));
+
+            if (circuitWithIndex1 is {Count: > 0} && circuitWithIndex2 is {Count: > 0})
+            {
+                if (circuitWithIndex1 == circuitWithIndex2)
+                    continue;
+
+                circuitWithIndex1.UnionWith(circuitWithIndex2);
+                circuits.Remove(circuitWithIndex2);
+                if (circuits.Count == 1 && circuits[0].Count == input.Length)
+                    return input[index1][0] * input[index2][0];
+
+                continue;
+            }
+
+            if (circuitWithIndex1 is {Count: > 0})
+            {
+                circuitWithIndex1.Add(index2);
+                if (circuits.Count == 1 && circuits[0].Count == input.Length)
+                    return input[index1][0] * input[index2][0];
+                continue;
+            }
+
+            if (circuitWithIndex2 is {Count: > 0})
+            {
+                circuitWithIndex2.Add(index1);
+                if (circuits.Count == 1 && circuits[0].Count == input.Length)
+                    return input[index1][0] * input[index2][0];
+                continue;
+            }
+
+            circuits.Add(new HashSet<int> { index1, index2 });
+        }
+
+        return 0L;
     }
 }
