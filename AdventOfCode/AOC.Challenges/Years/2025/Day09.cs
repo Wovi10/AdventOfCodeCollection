ad8ec1e00be4e2ed7e51bf7e6071fb224d06e55b
Wout Vinckevleugel <wout.vinckevleugel@gmail.com>
2026-08-05 10:51:57 +0200

    Prepared day 9

---
+++ AdventOfCode/AOC.Challenges/Years/2025/Day09.cs
@@ -0,0 +1,16 @@
+using AOC.Utils;
+
+namespace AOC.Challenges.Years._2025;
+
+public class Day09() : DayBase("09", "Movie Theater")
+{
+    protected override Task<object> PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<object> PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
